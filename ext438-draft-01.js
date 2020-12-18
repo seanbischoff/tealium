80221bad4563a10232484d096415615afa9a7e46
seanbischoff <seanb333@gmail.com>
2020-12-17 18:32:51 -0800

    Update ext438-draft-01.js
    Edit the file and save to test Tealium file syncing.

--- ext438-draft-01.js
+++ ext438-draft-01.js
@@ -1,8 +1,10 @@
 if (window.ccpaShareAdvertising && typeof window.pickedSubscribedCIQAdvertising === 'undefined') {
     window.pickedSubscribedCIQAdvertising = true;
     //This is identical to  Tealium Draft 99 draft 1, but is hosted on private github repo seanbischoff/tealium
+    console.log('Set Listener for Picked Subscribe, for Causal IQ');
     $('body').on('pickedSubscribed.pickedSubscribedCIQAdvertising', function(evt, data) {
         try {
+            console.log('Track Picked Subscribe for Causal IQ');
             /* Their example SRC:
             <img src='
             [https://gwmtracking.com/p/v/1/5f1b2a87f8708149cf219103/format/img?v1=[TargetBoxPrice]&v2=[BoxFrequency]
