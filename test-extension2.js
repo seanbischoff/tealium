9db7fdd4d5ccbb1fc03d71cd9c63afd96c9f4ad2
Sean Bischoff <sbischoff@wine.com>
2021-01-22 16:35:48 -0800

    Patch-1 edit the extension2 file

--- test-extension2.js
+++ test-extension2.js
@@ -1,4 +1,4 @@
 if (window.testExtension2 && typeof window.testExtension2 === 'undefined') {
     window.testExtension2 = true;
-    console.log('Tealium Extension loaded, source: github');
+    console.log('Tealium Extension 2 edited locally and saved. Loaded, source: github');
 }
